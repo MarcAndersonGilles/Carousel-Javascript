e926a97202826f687c6f4d4299f33be2506ec32e
marc <hornil6748@gmail.com>
2023-09-10 22:33:06 -0400

    ajout du code pour changement image

--- main.js
+++ main.js
@@ -1,29 +1,54 @@
+
+const arrayImages = [
+    {
+        1: { id: 1, title: "image1", href: "https://picsum.photos/200/300" },
+        2: { id: 2, title: "image2", href: "https://picsum.photos/200/300" },
+        3: { id: 3, title: "image3", href: "https://picsum.photos/200/300" },
+        4: { id: 4, title: "image4", href: "https://picsum.photos/200/300" },
+        5: { id: 5, title: "image5", href: "https://picsum.photos/200/300" },
+        6: { id: 6, title: "image6", href: "https://picsum.photos/200/300" },
+    }
+]
+const arrayImagesLength = Object.keys(arrayImages[0]).length ;
+
 const upItem = document.querySelector(".upItem")
 const downItem = document.querySelector(".downItem")
 
 const initialCarouselNumber = document.querySelector(".initialCarouselNumber")
 const finalCarouselNumber = document.querySelector(".finalCarouselNumber")
-finalCarouselNumber.textContent = 30
-finalCarousel = 30;
+getImage = document.querySelector(".arrayImages")
+finalCarouselNumber.textContent = arrayImagesLength;
+finalCarousel = arrayImagesLength;
 
-let initialNumber = 0;
-let currentNumber = initialNumber ;
+let initialNumber = 1;
+
+let currentNumber = initialNumber;
 initialCarouselNumber.textContent = currentNumber
 
-upItem.addEventListener("click", () => {
-    
-  
 
- if(currentNumber < finalCarousel){
-    currentNumber += 1
-    initialCarouselNumber.textContent = currentNumber
-}
+
+
+getImage.textContent = arrayImages[0][1].title;
+alert(arrayImages[0][1].title)
+
+upItem.addEventListener("click", () => {
+
+    if (currentNumber < finalCarousel ) {
+        currentNumber += 1
+        initialCarouselNumber.textContent = currentNumber
+        let currentImageUp = arrayImages[0][currentNumber].title;
+        getImage.textContent = currentImageUp;
+
+    }
 })
+
 downItem.addEventListener("click", () => {
-    
- if(currentNumber > 0){
-    currentNumber -= 1
-    initialCarouselNumber.textContent = currentNumber
-}
+
+    if (currentNumber > 1) {
+        currentNumber -= 1
+        initialCarouselNumber.textContent = currentNumber
+        let currentImageDown = arrayImages[0][currentNumber].title;
+        getImage.textContent = currentImageDown;
+    }
 })
 
