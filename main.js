11446b371db7afa4f31b450d76294a15614d4a60
marc <hornil6748@gmail.com>
2023-09-12 22:14:18 -0400

    change photos with clicks

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 
 const arrayImages = [
     {
-        1: { id: 1, name:"Marc", title: "image1", href: "https://picsum.photos/200/300" },
-        2: { id: 2, name:"Jorg", title: "image2", href: "https://picsum.photos/200/300" },
-        3: { id: 3, name:"Alice", title: "image3", href: "https://picsum.photos/200/300" },
-        4: { id: 4, name:"Bob", title: "image4", href: "https://picsum.photos/200/300" },
-        5: { id: 5, name:"Lea", title: "image5", href: "https://picsum.photos/200/300" },
-        6: { id: 6, name:"Alyssa", title: "image6", href: "https://picsum.photos/200/300" },
+        1: { id: 1, name: "Marc", title: "image1", href: "img/bob.png" },
+        2: { id: 2, name: "Jorg", title: "image2", href: "img/alyssa.png" },
+        3: { id: 3, name: "Alice", title: "image3", href: "img/eric.png" },
+        4: { id: 4, name: "Bob", title: "image4", href: "img/maya.png" },
+        5: { id: 5, name: "Lea", title: "image5", href: "img/marc.png" },
+        6: { id: 6, name: "Alyssa", title: "image6", href: "img/alice.png" },
     }
 ]
-const arrayImagesLength = Object.keys(arrayImages[0]).length ;
+const arrayImagesLength = Object.keys(arrayImages[0]).length;
 
 const upItem = document.querySelector(".upItem")
 const downItem = document.querySelector(".downItem")
 
 const initialCarouselNumber = document.querySelector(".initialCarouselNumber")
 const finalCarouselNumber = document.querySelector(".finalCarouselNumber")
+
+
 const nameUser = document.querySelector(".name")
 getImage = document.querySelector(".arrayImages")
+
 finalCarouselNumber.textContent = arrayImagesLength;
+
 
 finalCarousel = arrayImagesLength;
 
@@ -27,7 +31,9 @@ let initialNumber = 1;
 let currentNumber = initialNumber;
 initialCarouselNumber.textContent = currentNumber
 
-
+const imageUser = document.getElementById("#imageUser");
+console.log(imageUser)
+imageUser.setAttribute("src", arrayImages[0][1].href);
 
 
 getImage.textContent = arrayImages[0][1].title;
@@ -35,12 +41,12 @@ nameUser.textContent = arrayImages[0][1].name;
 
 upItem.addEventListener("click", () => {
 
-    if (currentNumber < finalCarousel ) {
+    if (currentNumber < finalCarousel) {
         currentNumber += 1
         initialCarouselNumber.textContent = currentNumber
-        let currentImageUp = arrayImages[0][currentNumber].title;
+        imageUser.setAttribute("src", arrayImages[0][currentNumber].href);
         nameUser.textContent = arrayImages[0][currentNumber].name;
-        getImage.textContent = currentImageUp;
+        getImage.textContent = arrayImages[0][currentNumber].title;
 
     }
 })
@@ -50,8 +56,10 @@ downItem.addEventListener("click", () => {
     if (currentNumber > 1) {
         currentNumber -= 1
         initialCarouselNumber.textContent = currentNumber
-        let currentImageDown = arrayImages[0][currentNumber].title;
-        getImage.textContent = currentImageDown;
+        // let currentImageDown = arrayImages[0][currentNumber].title;
+        imageUser.setAttribute("src", arrayImages[0][currentNumber].href);
+        nameUser.textContent = arrayImages[0][currentNumber].name;
+        getImage.textContent = arrayImages[0][currentNumber].title
     }
 })
 
